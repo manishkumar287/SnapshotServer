fb68e8f3f4d5a6d7b3d65e4094f49621703018f6
manishkumar287 <manishkumarchaudhary287@gmail.com>
2022-07-06 15:49:33 +0530

    server

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL = "mongodb+srv://manishkumar287:manishkumar287@cluster0.1lsva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+//mongodb+srv://manishkumar287:<password>@cluster0.1lsva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+
+
+
+
+
+
+
+// mongoose.set('useFindAndModify', false);
+
+
